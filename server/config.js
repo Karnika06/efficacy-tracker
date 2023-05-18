b76d99e2ee15040c084a3b3985d62359b642c42b
Karnika06 <karnika621@gmail.com>
2023-05-18 15:31:10 +0530

    Added env and config files

---
+++ server/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  JWT_SECRET: "9It0vof6tVeWWRYiHw0rv/yU2REmdaBGqRa/MXOD+SI=",
+  HOST: "smtp.gmail.com",
+  EMAIL_SERVICE: "gmail",
+  EMAIL_PORT: "587",
+  SECURE: true,
+  EMAIL: "efficacytracker@gmail.com",
+  PASS: "fkphcnybylpfbpir",
+  USER_DB: "root",
+
+  HOST_DB: "localhost",
+
+  PASSWORD_DB: "Karnika126@",
+
+  DATABASE_DB: "efficacytracker",
+
+  SALT_ROUNDS: 10,
+
+  SERVER_PORT: 3001
+};
